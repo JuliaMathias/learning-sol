8a90abb021cc3c6e6fe3d7d79f0ce09b625c3727
JuliaMathias <juliamss@gmail.com>
2022-10-07 15:15:18 -0300

    create FundMe contract

---
+++ .workspaces/default_workspace/contracts/Lesson 4/FundMe.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+// Objectives
+// 1. Get funds from users
+// 2. Withdraw funds
+// 3. Set a minimum funding value in USD
+
+contract FundMe {
+    function fund() public {
+        // Want to be able to set a minimum fund amount in USD
+        // 1. How do we send ETH to this contract?
+    }
+
+    // function withdraw() {}
+}
