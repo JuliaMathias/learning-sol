ec690e8a3a93aec0e80f6195a250eef7c806c553
JuliaMathias <juliamss@gmail.com>
2022-10-07 15:41:32 -0300

    require a minimum of ETH spent

--- .workspaces/default_workspace/contracts/Lesson 4/FundMe.sol
+++ .workspaces/default_workspace/contracts/Lesson 4/FundMe.sol
@@ -7,9 +7,10 @@ pragma solidity ^0.8.8;
 // 3. Set a minimum funding value in USD
 
 contract FundMe {
-    function fund() public {
+    function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this contract?
+        require(msg.value > 1e18, "You need to spend at least 1 ETH");
     }
 
     // function withdraw() {}
--- notes/Lesson 4: Remix Fund Me/notes.md
+++ notes/Lesson 4: Remix Fund Me/notes.md
@@ -5,3 +5,5 @@
 ![transaction fields](transaction-fields.png)
 ![transaction values](transaction-values.png)
 ![transaction functions](transaction-functions.png)
+
+- Smart contracts can hold funds, just like wallets
