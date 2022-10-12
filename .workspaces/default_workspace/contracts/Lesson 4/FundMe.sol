085a4781a24972edbb9267b3a15de2da2073d71e
JuliaMathias <juliamss@gmail.com>
2022-10-12 10:23:48 -0300

    explain contract reversal

--- .workspaces/default_workspace/contracts/Lesson 4/FundMe.sol
+++ .workspaces/default_workspace/contracts/Lesson 4/FundMe.sol
@@ -11,6 +11,10 @@ contract FundMe {
         // Want to be able to set a minimum fund amount in USD
         // 1. How do we send ETH to this contract?
         require(msg.value > 1e18, "You need to spend at least 1 ETH");
+
+        // if the conditions on the require above are not met, any gas spent doing
+        // computations **after** the require statement will be refunded to the user but the
+        // gas spent before the require statement will not be refunded
     }
 
     // function withdraw() {}
